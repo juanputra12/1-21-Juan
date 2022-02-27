9dd09d161aa55be6ea855427a431f191cd15c23b
juanputra12 <juanputra154@gmail.com>
2022-02-26 19:01:13 -0800

    Modul 3

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "about",
+        "nama" => "Juan Putra Hardiansyah",
+        "email" => "juanputra154@gmail.com",
+        "gambar" => "juan.jpg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
+
+use App\Http\Controllers\ContactController;
+Route::resource('/contacts', ContactController::class);
